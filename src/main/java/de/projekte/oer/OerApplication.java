ae06d7521fef9683e589420551c47e1c1cf66653
Muhammad <1737353@uni-wuppertal.de>
2025-02-28 01:11:49 +0100

    Add CommandLineRunner to create default admin user and roles

--- src/main/java/de/projekte/oer/OerApplication.java
+++ src/main/java/de/projekte/oer/OerApplication.java
@@ -1,7 +1,14 @@
 package de.projekte.oer;
 
+import de.projekte.oer.dao.MemberRepository;
+import de.projekte.oer.dao.RoleRepository;
+import de.projekte.oer.entity.Member;
+import de.projekte.oer.entity.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class OerApplication {
@@ -10,4 +17,35 @@ public class OerApplication {
 		SpringApplication.run(OerApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner createDefaultAdmin(MemberRepository memberRepository,
+												RoleRepository roleRepository,
+												PasswordEncoder passwordEncoder) {
+		return args -> {
+			String adminUserId = "admin";
+			if (!memberRepository.existsById(adminUserId)) {
+				Member admin = new Member();
+				admin.setUserId(adminUserId);
+				// Standard-Passwort "admin", verschlüsselt mit dem delegierenden PasswordEncoder
+				admin.setPw(passwordEncoder.encode("admin"));
+				admin.setActive(true);
+				memberRepository.save(admin);
+
+				Role adminRole = new Role();
+				adminRole.setUserId(adminUserId);
+				adminRole.setRole("ROLE_ADMIN");
+				roleRepository.save(adminRole);
+
+				Role managerRole = new Role();
+				managerRole.setUserId(adminUserId);
+				managerRole.setRole("ROLE_MANAGER");
+				roleRepository.save(managerRole);
+
+				Role employeeRole = new Role();
+				employeeRole.setUserId(adminUserId);
+				employeeRole.setRole("ROLE_EMPLOYEE");
+				roleRepository.save(employeeRole);
+			}
+		};
+	}
 }
